9526418ad88c74a44c1dc27a76127b01dcc42307
Mariluz <mluzco29@gmail.com>
2024-11-19 16:15:16 +0100

    done

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,23 @@
-function divide () {
-       
+function divide (a,b) {
+    if (a === undefined || b === undefined) {
+        return undefined;
+    }
+    return a / b;
 }
+
+
+describe("divide", function() {
+    it("should be defined", function() {
+        expect(divide).toBeDefined();
+    });
+
+    it("should return the division of two numbers", function() {
+        expect(divide(6, 3)).toBe(2);
+    });
+
+    it("should return undefined if any argument is missing", function() {
+        expect(divide(6)).toBeUndefined();
+        expect(divide(undefined, 3)).toBeUndefined();
+    });
+});
+       
